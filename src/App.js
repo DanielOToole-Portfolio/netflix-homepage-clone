5f46a920ec044c7c1be798daa0879a1c5beb42f9
Daniel O'Toole <danielotoole0@gmail.com>
2021-11-14 11:20:35 +0000

    commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Top Romance Picks" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
